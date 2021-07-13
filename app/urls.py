61a86fe992a61afe63ead8c0f377ddcacb5759f2
jenmcdonnell <jennymcdonn@gmail.com>
2021-07-13 18:34:06 -0400

    login view/template and fixed secret key

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage, name="home"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
