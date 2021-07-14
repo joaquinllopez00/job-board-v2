5db4b5303d9918a9317cd09a34228989d6eb6d93
joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-07-14 00:13:23 -0400

    Implemented old styles, implemented new Listing Model, Listing View, Listing Form.  Had issues with signing up and creating users so for now they aren't tied to a user, all logic is commented out and waiting for user/authentication to be functional

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.http import request
 from django.urls import path
 from user import views
+from job import views as job_views
 from django.conf.urls import (handler404, handler500)
 
 handler404 = views.handler404
@@ -25,6 +26,8 @@ handler500 = views.handler500
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.homepage, name="home"),
+    path('create/', job_views.create_listing_view.as_view(), name="create_listing"),
+    path('listing/<int:listing_id>/', job_views.listing_detail_view.as_view(), name="listing_detail"),
     path("signup/", views.signup_view, name="signup"),
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
--- job/views.py
+++ job/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
+from .models import Listing
+from .forms import CreateListingForm
 # Create your views here.
+class listing_detail_view(View):
+
+    def get(self, request, *args, **kwargs):
+        listing_id = kwargs['listing_id']
+        template = 'listing_detail.html'
+        listing = Listing.objects.get(id=listing_id)
+        context = {
+            'listing': listing
+        }
+        return render(request, template, context)
+
+class create_listing_view(View):
+    def get(self, request):
+        user = self.request.user
+        template_name = 'create_listing.html'
+        form = CreateListingForm()
+        context = {'form': form}
+        return render(request, template_name, context)
+    def post(self, request, *args, **kwargs):
+        form = CreateListingForm(request.POST)
+        if request.method == 'POST':
+            if form.is_valid():
+                listing = form.save()
+                # instance.user = user
+                listing.save()
+                template = 'listing_detail.html'
+                messages.add_message(request, messages.INFO, "<p id='listing-message'>Want to create another Job Listing? Click <a href='/create/'>here</a></p>", extra_tags='safe')
+                return HttpResponseRedirect("/listing/%s" % listing.id)
