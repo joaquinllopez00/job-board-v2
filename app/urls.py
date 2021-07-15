c78271a3379912c2ac729908effb7654190f6d6c
jenmcdonnell <jennymcdonn@gmail.com>
2021-07-15 11:31:38 -0400

    changed url arg to <str:username>

--- app/urls.py
+++ app/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
-    path('profile/<username>', views.profile_view, name="profile"),
+    path('profile/<str:username>', views.profile_view, name="profile"),
     path('favorite/<int:listing_id>/',
          job_views.toggle_favorite, name="favorite"),
 ]
